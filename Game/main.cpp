abcdcc2381e066dc7ea569c25cdd029a7b0e0d9d
tatannjs <ethan.eveilleaux@gmail.com>
2026-03-02 23:34:13 +0100

    Ajoutez des fichiers projet.

---
+++ Game/main.cpp
@@ -0,0 +1,16 @@
+#include <OGL3D/Game/OGame.h>
+#include <iostream>
+
+int main() {
+
+	try {
+		OGame game;
+		game.run();
+	}
+	catch(const std::exception& e)
+	{
+		std::cout << "Error: " << e.what() << std::endl;
+		return 1;
+	}
+	return 0;
+}
---
+++ OGL3D/include/OGL3D/Game/OGame.h
@@ -0,0 +1,23 @@
+#include <memory>
+
+class OGraphicsEngine;
+class OWindow;
+class OGame
+{
+public:
+	OGame();
+	~OGame();
+
+	virtual void onCreate();
+	virtual void onUpdate();
+	virtual void onQuit();
+
+	void run();
+	void quit();
+
+protected:
+	bool m_isRunning = true;
+	std::unique_ptr<OGraphicsEngine> m_graphicsEngine;
+	std::unique_ptr<OWindow> m_display;
+};
+
